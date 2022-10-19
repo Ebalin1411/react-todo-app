1229f1ed2b86c68d0bdc74c706e2653137b88410
Ebalin1411 <ebalinrosecse2006@gmail.com>
2022-10-19 10:04:29 +0530

    feat: todo items added

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,58 @@ import TodoItem, { Todo } from './component/todo-item';
 
 
 function App() {
- //Creating new state for list item
- const [todoText,setTodoText]=useState<string>('');  //state Declaration
-//State for List
-const [todoList,setTodoList]=useState<Array<Todo>>([]);
+        //Creating new state for list item
+    const [todoText,setTodoText]=useState<string>('');  //state Declaration
+        //State for List
+    const [todoList,setTodoList]=useState<Todo[]>([]);
 
-const onTodoChange=(e:any)=>{
-    setTodoText(e.target.value);
-}
-
-const onTodoKeyDown=(e:any)=>{
-    if(e.keyCode ===13){
-      //Add item to the list
-
-      setTodoList(
-        todoList.concat({ 
-          name: todoText,
-          isDone:false, 
-        })
-      ); 
-    }
-}
-  return (
-   <div className="container flex flex-col item-center gap-5 m-20">
-    <div className='flex flex-col '>
-        <h1>My ToDo List</h1>
-        <input 
-        type="text"
-        onChange={onTodoChange}
-        onKeyDown={onTodoKeyDown}
-        value={todoText}
-        className="border-2 " 
-        placeholder="Enter Task Name to Add"></input>
-        {/*list of to do */}
-    </div>
-    <div id="todoList" className='flex flex-col justify-evenly '>
-
-        {todoList.map((todo:any)=>
-        <TodoItem todo={todo} /> )}  
+    const onTodoChange=(e:any)=>{
+        setTodoText(e.target.value);
         
- 
+    }
 
+    const onTodoKeyDown=(e:any)=>{
+        if(e.keyCode ===13){
+          //Add item to the list
+          setTodoText("")
+          setTodoList(
+            
+            todoList.concat({ 
+              name: todoText,
+              isDone:false, 
+            })
+          ); 
+        }
+
+
+        const handleDelete =(id:number)=>{
+          console.log(TodoItem);
+
+        }
+    }
+      return (
+      <div className="container flex flex-col item-center gap-5 m-20  bg-slate-400 h-screen w-auto ">
+        <div className='flex flex-col m-10  '>
+            <h1 className='text-2xl text-center m-8'>My ToDo List</h1>
+            <input 
+                type="text"
+                onChange={onTodoChange}
+                onKeyDown={onTodoKeyDown}
+                value={todoText}
+                className="border-2 m-2 " 
+                placeholder="Enter Task Name to Add">
+            </input>
+            {/*list of to do */}
+            <div id="todoList" className='flex flex-col m-2 '>
+            {todoList.map((todo,index)=>
+            <TodoItem  key ={index} todo={todo} /> )}            
+             
+      </div>
+      </div>
+      
     </div>
-
-   
-   </div>
-   
-  );
-}
+      
+      );
+  }
 
 export default App;
--- src/component/todo-item.tsx
+++ src/component/todo-item.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
-
+import {AiFillDelete} from 'react-icons/ai'
 
 interface Props{
     todo:Todo
 }
 
-export interface Todo{
+export interface Todo{ 
+    id ?: number   
     isDone:boolean;
     name:string;
+    handleDelete?:Function;
 }
 
 //Function component using Hook
 const TodoItem = (props:Props)=>{
 
     return(            //html elements
-        <div className="flex gap-5">
-            <input type="checkbox" checked={props.todo.isDone} />
-            <label>{props.todo.name}</label>
-            <button>Delete</button>
+        <div className="flex items-center space-x-10">
+            <input type="checkbox" checked={props.todo.isDone}/>
+            <label id="">{props.todo.name}</label>           
+            <span ><AiFillDelete/></span>               
         </div>
     )
 
