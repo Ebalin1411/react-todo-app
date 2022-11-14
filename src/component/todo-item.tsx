0ff59275e1026126361eab40a8bb015cff71c49c
Ebalin1411 <ebalinrosecse2006@gmail.com>
2022-11-14 20:37:13 +0530

    feat

--- src/component/todo-item.tsx
+++ src/component/todo-item.tsx
@@ -27,17 +27,22 @@ const TodoItem = (props:Props)=>{
    const [isTextEditable, setIsTextEditable] = useState<boolean>(false);
 
    const   handleTextChange =(e:any)=> {
-        if(e.keyCode === 13){
-            alert('inside HandleTextChange'+ e.target.value + props.todo.id)
-            setIsTextEditable(false);
+    props.onTextChange(e.target.value)
+    alert('inside HandleTextChange'+ e.target.value + props.todo.id)
+        // if(e.keyCode === 13){
            
-        return;
-        }
-        props.onTextChange(e.target.value)
+        //     setIsTextEditable(false);
+           
+        // return;
+        // }
+       
        
    }
+
+
    const onTextKeyPress =async(e:any)=> {
     if(e.keyCode === 13){
+        setIsTextEditable(false);
         alert('insideOnTextKeyPress'+ e.target.value) //Update
         alert('update id'+ props.todo.id)
         const apiUrl =`https://elscmcoswajrkdazeklc.supabase.co/rest/v1/Todos/${props.todo.id}`;
@@ -47,20 +52,13 @@ const TodoItem = (props:Props)=>{
                 Authorization: `Bearer ${APIToken}`
               }
     
-        });
-       
-
-
-     
-        setIsTextEditable(false);
+        }); 
         
     return;
 
     
      }
-
-
-    }
+    };
 
 
     return(            //html elements
